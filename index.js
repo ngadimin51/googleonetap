4bc51b5f61b5ccfb151b46cdca18a68db949aba4
ngadimin51 <poconggoyangkayang@gmail.com>
2021-07-13 17:28:46 +0700

    commit on main

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const http = require('http')
 const server = http.createServer(app)
 const path = require('path')
 
-app.get('/*', (req, res) => {
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
